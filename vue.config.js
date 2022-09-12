1280edd40ee6036a34fe2c42a11613f369348e90
gudu <3072953029@qq.com>
2022-09-12 13:50:23 +0800

    增加生产坏境代码混淆配置

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const Obfuscator = require('webpack-obfuscator');
 module.exports = {
     publicPath: './',
     lintOnSave: false,
     devServer: {
         proxy: 'http://127.0.0.1:3000'
     },
+    productionSourceMap: false,
     chainWebpack(config) {
 
         console.log(process.env.NODE_ENV);
@@ -16,6 +18,15 @@ module.exports = {
                 'vue': 'Vue',
                 'vue-router': 'VueRouter'
             });
+
+            config.plugin('obfuscator').use(Obfuscator, [{
+                compact: true,
+                stringArray: true,
+                stringArrayEncoding: ['rc4'],
+                stringArrayThreshold: 1,
+                transformObjectKeys: true,
+                unicodeEscapeSequence: false
+            }]);
 
             config.entry('app').clear().add(path.join(__dirname, "./src/main-prod.js"));
             config.plugin('html').tap(args => {
